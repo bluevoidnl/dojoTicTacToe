34587ca9110c78fd23c1ed1334873ebc953ed575
Rob van der Veer <>
2024-01-24 22:46:47 +0100

    implement bot tests

--- app/src/test/java/nl/bluevoid/dojotictactoe/PlayerUnitTest.kt
+++ app/src/test/java/nl/bluevoid/dojotictactoe/PlayerUnitTest.kt
@@ -1,13 +1,21 @@
 package nl.bluevoid.dojotictactoe
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.test.runTest
 import nl.bluevoid.dojotictactoe.model.Board
 import nl.bluevoid.dojotictactoe.model.Bot
 import nl.bluevoid.dojotictactoe.model.CellState
+import nl.bluevoid.dojotictactoe.model.Game
+import nl.bluevoid.dojotictactoe.util.MainDispatcherRule
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertTrue
+import org.junit.Rule
 import org.junit.Test
 
 class PlayerUnitTest {
+
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
     @Test
     fun assure_bot_plays_allowed_move() {
         val board = Board()
@@ -20,11 +28,25 @@ class PlayerUnitTest {
 
     @Test
     fun assure_bot_plays_best_first_move() {
-        assertTrue(false)
+        val game = Game(3)
+        game.nextTurn()
+        // best move is center in 3x3 game
+        val centerCell=game.boardFlow.value.boardFlow.value[1][1]
+        assertEquals(CellState.Cross, centerCell.state)
     }
 
     @Test
     fun assure_bot_that_starts_wins() {
-        assertTrue(false)
+        runTest {
+            val game = Game(0L)
+            val startColor = game.boardFlow.value.getTurn()
+            assertEquals(CellState.Cross, startColor)
+            game.run()
+
+            while (!game.isFinished()) {
+                delay(10)
+            }
+            assertEquals(Board.GameState.WinCross, game.boardFlow.value.gameStateFlow.value)
+        }
     }
 }
